0769f76c27e5e495fd633ae04f056bb4c1fcfc03
Suganya <sugandinesh25@gmail.com>
2025-05-20 02:58:01 +0530

    websocket testing

--- app/ws_manager.py
+++ app/ws_manager.py
@@ -1,7 +1,8 @@
 from typing import Set
 from fastapi import WebSocket
 from app.constants import STATUS_NO_BREAKOUT, WS_MSG_TYPE_THRESHOLD_UPDATE
-
+import asyncio
+import random
 # Use a set for better performance and safety
 connected_clients: Set[WebSocket] = set()
 
@@ -114,3 +115,24 @@ async def broadcast_single_print(result: dict):
         except Exception as e:
             connected_clients.discard(client)
             print(f"❌ Removed client due to error: {e}")
+
+
+
+async def broadcast_random_counter():
+    counter = 0
+    while True:
+        if connected_clients:
+            message = {
+                "type": "random_counter",
+                "counter": counter,
+                "randomValue": random.randint(1, 100)
+            }
+            print(f"📤 Broadcasting Random Counter: {message} to {len(connected_clients)} clients")
+            for client in connected_clients.copy():
+                try:
+                    await client.send_json(message)
+                except Exception as e:
+                    connected_clients.discard(client)
+                    print(f"❌ Removed client due to error: {e}")
+        counter += 1
+        await asyncio.sleep(5)  # broadcast every 5 seconds
